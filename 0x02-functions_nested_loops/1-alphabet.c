2b87fecb82796f18be33e25745a7b4a33bf001ae
ronsig30 <ronaldrogony30@gmail.com>
2023-07-18 06:04:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Description - Printing alphabets in lower case
+ * Return - Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
