a26a168986ec8b2af585b7a0677b1ffd112ac010
ronsig30 <ronaldrogony30@gmail.com>
2023-07-20 10:52:20 -0700

    Lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - straight line
+ * @n: number of times
+ * Return: Alway 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
