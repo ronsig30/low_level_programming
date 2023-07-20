d5fff15dc7c18c4af45db7acab3fec5c8adf9292
ronsig30 <ronaldrogony30@gmail.com>
2023-07-20 02:43:22 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _isupper - Prints uppercase
+ * @c: uppercase
+ * Return: 1 if upper, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c  <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
