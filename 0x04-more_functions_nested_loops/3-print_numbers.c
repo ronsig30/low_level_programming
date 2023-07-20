3925d56ce2e2d91c76175887d9993f0357abf0c7
ronsig30 <ronaldrogony30@gmail.com>
2023-07-20 04:41:19 -0700

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - outputs numbers
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
