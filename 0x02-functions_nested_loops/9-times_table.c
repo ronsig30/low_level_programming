c8b48769bc40cf3fd0ca5ecdd1943c69e02bc31b
ronsig30 <ronaldrogony30@gmail.com>
2023-07-25 05:10:35 -0700

    9 Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,32 @@
  */
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int i, j, k;
 
-	for (x = 0; x <= 9; x++)
+	for (i = 0; i < 10; i++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (j = 0; j < 10; j++)
 		{
-			z = x * y;
-			if (z > 9)
+			k = j * i;
+			if (j == 0)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar(k + '0');
 			}
-			else
-			if (y != 0)
+			if (k < 10 && j != 0)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			_putchar(z + '0');
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
