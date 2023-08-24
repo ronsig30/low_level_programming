87e02ad9ea9031336432db72c5c6a8e1f09299d7
ronsig30 <ronaldrogony30@gmail.com>
2023-08-24 15:21:38 -0700

    Hello_hoberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
-
-section .text
-    global main
-
-extern printf
-
-main:
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-
-    pop rbp
-    ret
+ global    main 
+           extern    printf 
+ main: 
+           mov   edi, format 
+           xor   eax, eax 
+           call  printf 
+           mov         eax, 0 
+           ret 
+ format: db `Hello, Holberton\n`,0
